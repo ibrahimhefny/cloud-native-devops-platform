c9e5a6b77e3d3ce01e7a2a8807f691097ce85f7c
Ibrahim Hefny <ibrahim.mo.hefny@gmail.com>
2026-02-10 18:17:11 +0200

    Organizing + Add API Gateway resources and methods for testing

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -1,10 +1,76 @@
-resource "aws_api_gateway_rest_api" "this" {
-  name = "${var.name_prefix}-api"
+############################
+# API Resource: /test
+############################
+resource "aws_api_gateway_resource" "test" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "test"
 }
 
-resource "aws_api_gateway_authorizer" "cognito" {
-  name          = "cognito-authorizer"
+############################
+# GET Method (Cognito protected)
+############################
+resource "aws_api_gateway_method" "test_get" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  type          = "COGNITO_USER_POOLS"
-  provider_arns = [var.cognito_user_pool_arn]
+  resource_id   = aws_api_gateway_resource.test.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito.id
+}
+
+############################
+# Mock Integration
+############################
+resource "aws_api_gateway_integration" "test_mock" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.test_get.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+############################
+# Method Response
+############################
+resource "aws_api_gateway_method_response" "test_200" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.test_get.http_method
+  status_code = "200"
+}
+
+############################
+# Integration Response
+############################
+resource "aws_api_gateway_integration_response" "test_200" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.test_get.http_method
+  status_code = aws_api_gateway_method_response.test_200.status_code
+
+  response_templates = {
+    "application/json" = jsonencode({
+      message = "Hello from protected API"
+    })
+  }
+}
+
+############################
+# Deployment + Stage
+############################
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  depends_on = [
+    aws_api_gateway_integration.test_mock
+  ]
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  deployment_id = aws_api_gateway_deployment.this.id
+  stage_name    = "dev"
 }
